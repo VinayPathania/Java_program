3186e80ebc6737c37b694fac059aa1547471daa6
VinayPathania <you@example.comvinaypathania2002@gmail.com>
2022-06-24 22:34:53 +0530

    static non static diff added

---
+++ src/Day2/Cricket_score.java
@@ -0,0 +1,7 @@
+package Day2;
+
+public class Cricket_score {
+    public static void main(String[] args) {
+        System.out.println("Vinay");
+    }
+}
---
+++ src/Day3/Static_NonStatic.java
@@ -0,0 +1,31 @@
+package Day3;
+
+public class Static_NonStatic {
+    static int sum(){
+        return 4 + 6;
+    }
+
+    int div (){
+        return 20 / 3;
+    }
+
+    public static void main(String[] args) {
+
+        //Static method vs Non-static method
+        //The Common functionality of all objects in the application must be defined as
+        //static.
+        //Non-static functionality belongs to a particular object.
+
+        int c = sum();
+        System.out.println(c);
+
+        Static_NonStatic obj = new Static_NonStatic();
+
+        int z = obj.div();
+
+        System.out.println(z);
+
+
+
+    }
+}
