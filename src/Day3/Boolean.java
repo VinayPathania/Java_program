e23ede018e681fcaf2a23124e502d41a362a692d
VinayPathania <you@example.comvinaypathania2002@gmail.com>
2022-06-25 16:23:28 +0530

    day3 Q2

---
+++ src/Day3/Boolean.java
@@ -0,0 +1,23 @@
+package Day3;
+
+import java.util.Scanner;
+
+public class Boolean {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("What is happening outside is it raining?");
+        boolean isRaining = sc.nextBoolean();
+        System.out.println("Is it snowing?");
+        boolean isSnowing = sc.nextBoolean();
+        System.out.println("What is the temp?");
+        double temp = sc.nextDouble();
+
+
+        if(isRaining || isSnowing || temp<10){
+            System.out.println("Let's stay home");
+        }
+        else {
+            System.out.println("Let's go outside!");
+        }
+    }
+}
