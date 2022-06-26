522b9bd49bef8d03025457fda2756105cb05985a
VinayPathania <you@example.comvinaypathania2002@gmail.com>
2022-06-26 16:34:30 +0530

    day3 Q4

--- src/Day3/Student.java
+++ src/Day3/Student.java
@@ -1,22 +1,26 @@
 package Day3;
 
 import java.util.Scanner;
-
 public class Student {
 
+     static void studentDetail(String a, int b, int c){
+
+         System.out.println("Name: "+a);
+        System.out.println("Roll no: "+b);
+         System.out.println("Marks: "+c);
+    }
 
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Roll No");
+        System.out.println("Enter Roll no");
         int roll = sc.nextInt();
         System.out.println("Enter Name");
         String name = sc.next();
-        System.out.println("Enter Total Marks");
+        System.out.println("Enter Marks");
         int marks = sc.nextInt();
+        studentDetail(name,roll,marks);
+//        System.out.println(c);
 
-        System.out.println("Roll no:"+" "+roll);
-        System.out.println("Name:"+" "+name);
-        System.out.println("Marks:"+" "+marks);
     }
 }
