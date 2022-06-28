98ea821a2710f4d4e04cf42154fded99b4770fed
VinayPathania <you@example.comvinaypathania2002@gmail.com>
2022-06-28 09:24:27 +0530

    Cricket_score

--- src/Day2/Cricket_score.java
+++ src/Day2/Cricket_score.java
@@ -1,7 +1,23 @@
 package Day2;
 
+import java.util.Scanner;
+
 public class Cricket_score {
+    static int scoreCounter(int a, int b, int c, int d, int e){
+        b = b*2;
+        c = c*3;
+        d = d*4;
+        e = e*6;
+        return a+b+c+d+e;
+    }
     public static void main(String[] args) {
-        System.out.println("Vinay");
+        Scanner sc = new Scanner (System.in);
+        int ones = sc.nextInt();
+        int twos = sc.nextInt();
+        int threes = sc.nextInt();
+        int fours = sc.nextInt();
+        int sixes = sc.nextInt();
+        int result = scoreCounter(ones,twos,threes,fours,sixes);
+        System.out.println("Total score is "+result);
     }
 }
